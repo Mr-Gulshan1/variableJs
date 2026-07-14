97bba3f57eb34d4a0c2236630f61a4ddad79d3c6
Mr-Gulshan1 <nagalgulshan@gmail.com>
2026-07-14 21:28:08 +0300

    new variables

---
+++ newvariable.js
@@ -0,0 +1,19 @@
+// Values and their types
+
+let name = "Gulshan";
+console.log(name, typeof name);
+
+let age = 22;
+console.log(age, typeof age);
+
+let isStudent = true;
+console.log(isStudent, typeof isStudent);
+
+let salary = 25000.50;
+console.log(salary, typeof salary);
+
+let data = null;
+console.log(data, typeof data);
+
+let city;
+console.log(city, typeof city);
